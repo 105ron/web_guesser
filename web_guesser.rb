1377f72f3cca1c5da9ce260862fe37ac99ede568
rhys <rbrooker105@gmail.com>
2016-09-25 16:06:23 +1000

    I4 - App output string colorized

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,11 +3,12 @@ require 'sinatra/reloader'
 
 number = rand(100)
 response = ''
+color = 'blue'
 
 get '/' do
 	guess = params["guess"]
-	response = check_guess(guess, number)
-  erb :index, :locals => {:response => response}
+	response, color = check_guess(guess, number)
+  erb :index, :locals => {:response => response, :color => color}
 end
 
 
@@ -15,17 +16,16 @@ def check_guess(guess, number)
 	guess = guess.to_i unless guess == nil
 	number = number.to_i
 	if guess == nil
-		return "Guess the number"
+		return "Guess the number", "blue"
 	elsif guess == number
-		return "You got it. The number is #{guess}"
+		return "You got it. The number is #{guess}", "green"
 	elsif guess > (number + 5)
-		return "#{guess} is way too high"
+		return "#{guess} is way too high", "red"
 	elsif guess < (number - 5)
-		return "#{guess} is way too low"
+		return "#{guess} is way too low", "red"
 	elsif guess > number
-		return "#{guess} is too high"
+		return "#{guess} is too high", "yellow"
 	elsif guess < number
-		return "#{guess} is too low"
+		return "#{guess} is too low", "yellow"
 	end
-
 end
